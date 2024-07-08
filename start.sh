acd0822ce635732a8f02d5645cd34dabfc21874a
korayakpinar <akpnrkoray@gmail.com>
2024-07-08 23:45:28 +0300

    chore: added bash script to start node

---
+++ start.sh
@@ -0,0 +1,18 @@
+source ./config.toml
+
+echo "Building network & threshold-encryption"
+
+go build ./network/main.go
+cd threshold-encryption/
+cargo build
+cd ..
+
+echo "Downloading transcript"
+
+wget https://github.com/ethereum/kzg-ceremony/raw/main/transcript.json
+
+threshold-encryption/target/threshold-encryption --transcript transcript.json --blsKey $blsKey --port $apiPort & 
+echo "Api server started, waitin 15 seconds for initialization"
+sleep 15
+./network/main --port $port --privKey $privKey --rpc $rpc --port $proxyPort 
+
