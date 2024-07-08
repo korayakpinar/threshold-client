bf0ff68bbb2252c37dd731474b1d16b2601b33c5
korayakpinar <akpnrkoray@gmail.com>
2024-07-09 02:16:42 +0300

    feat: start.sh added

--- start.sh
+++ start.sh
@@ -11,7 +11,7 @@ echo "Downloading transcript"
 
 wget https://github.com/ethereum/kzg-ceremony/raw/main/transcript.json
 
-threshold-encryption/target/threshold-encryption --transcript transcript.json --blsKey $blsKey --port $apiPort & 
+threshold-encryption/target/threshold-encryption --transcript transcript.json --bls-key $blsKey --api-port $apiPort & 
 echo "Api server started, waitin 15 seconds for initialization"
 sleep 15
 ./network/main --port $port --privKey $privKey --rpc $rpc --port $proxyPort 
